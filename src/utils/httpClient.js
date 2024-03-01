497fb5775116293ed6c8ad3685450af9e850c6a7
Pedro H. Marques <phmarquesdev@gmail.com>
2024-02-29 23:35:28 -0300

    Added .env

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,11 +1,18 @@
 import axios from "axios";
 
+const baseURL =
+  import.meta.env.NODE_ENV === "production"
+    ? import.meta.env.VITE_PRODUCTION_API_URL
+    : import.meta.env.VITE_LOCAL_API_URL;
+
 const httpClient = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: baseURL,
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+console.log(baseURL);
 
 export default httpClient;
