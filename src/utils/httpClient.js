33d2fbd55544ee76382ef2cb66103d55f06b743a
Pedro H. Marques <phmarquesdev@gmail.com>
2024-02-29 23:43:14 -0300

    Fix .env

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const baseURL =
-  import.meta.env.NODE_ENV === "production"
-    ? import.meta.env.VITE_PRODUCTION_API_URL
-    : import.meta.env.VITE_LOCAL_API_URL;
+const baseURL = import.meta.env.VITE_API_URL;
 
 const httpClient = axios.create({
   baseURL: baseURL,
