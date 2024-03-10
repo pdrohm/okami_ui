e36114d9ef2ffe3e6d32fab4c40128c8dd5b3f34
Pedro H. Marques <phmarquesdev@gmail.com>
2024-03-10 16:52:13 -0300

    Add training and checkin tabs

--- src/utils/menuTabs.js
+++ src/utils/menuTabs.js
@@ -1,11 +1,16 @@
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
 import PaidIcon from "@mui/icons-material/Paid";
+import SportsKabaddiIcon from '@mui/icons-material/SportsKabaddi';
+import CheckIcon from '@mui/icons-material/Check';
 
 const menuTabs = [
   { name: "Dashboard", url: "/", icon: DashboardIcon },
   { name: "Alunos", url: "/alunos", icon: PeopleAltIcon },
+  { name: "Treinos", url: "/treino", icon: SportsKabaddiIcon },
+  { name: "Checkin", url: "/checkin", icon: CheckIcon },
   { name: "Financeiro", url: "/financeiro", icon: PaidIcon },
+
 ];
 
 export { menuTabs };
